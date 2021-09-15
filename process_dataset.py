ce16d28e28eb95246abec0eed06219facf7096d2
lobracost <pa.koromilas@gmail.com>
2021-09-15 23:51:08 +0300

    storing csd files to data directory

--- process_dataset.py
+++ process_dataset.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import sys
 
@@ -38,34 +39,36 @@ def deploy(in_dataset,destination):
 def download_data(dataset_config, dataset_name):
     source = {}
     dataset = {}
+    dataset_path = os.path.join("data", dataset_name)
+    folder_path = os.path.join(dataset_path, dataset_name)
 
-    if not os.path.isdir(dataset_name + "_raw"):
+    if not os.path.isdir(folder_path + "_raw"):
         source["raw"] = {'words': dataset_config.raw["words"]}
     else:
         print("Raw already exist, no need to download")
-        dataset["raw"] = mmdatasdk.mmdataset(dataset_name + "_raw")
+        dataset["raw"] = mmdatasdk.mmdataset(folder_path + "_raw")
 
-    if not os.path.isdir(dataset_name + "_highlevel"):
+    if not os.path.isdir(folder_path + "_highlevel"):
         tmp_dict = feature_selection[dataset_name]
         del tmp_dict["labels"]
         highlevel = {tmp_dict[key]: dataset_config.highlevel[tmp_dict[key]] for key in tmp_dict}
         source["highlevel"] = highlevel
     else:
         print("Highlevel features already exist, no need to download")
-        dataset["highlevel"] = mmdatasdk.mmdataset(dataset_name + "_highlevel")
+        dataset["highlevel"] = mmdatasdk.mmdataset(folder_path + "_highlevel")
 
-    if not os.path.isdir(dataset_name + "_labels"):
+    if not os.path.isdir(folder_path + "_labels"):
         source["labels"] = dataset_config.labels
     else:
         print("Labels already exist, no need to download")
-        dataset["labels"] = mmdatasdk.mmdataset(dataset_name + "_labels")
+        dataset["labels"] = mmdatasdk.mmdataset(folder_path + "_labels")
 
     # source = {"raw": dataset.raw, "highlevel": dataset.highlevel, "labels": dataset.labels}
     
     is_empty = not source
     if not is_empty:
         for key in source:
-            dataset[key] = mmdatasdk.mmdataset(source[key], dataset_name + "_" + key)
+            dataset[key] = mmdatasdk.mmdataset(source[key], folder_path + "_" + key)
 
     return dataset
 
@@ -73,29 +76,32 @@ def download_data(dataset_config, dataset_name):
 def process_data(dataset_name, seq_len=50):
 
     non_sequences = [feature_selection[dataset_name]["labels"]]
-    folders = [dataset_name + "_highlevel", dataset_name + "_labels"]
+
+    dataset_path = os.path.join("data", dataset_name)
+    folder_path = os.path.join(dataset_path, dataset_name)
+    folders = [folder_path + "_highlevel", folder_path + "_labels"]
 
     dataset={}
     for folder in folders:
         dataset[folder.split("_")[1]] = mmdatasdk.mmdataset(folder)
 
-    word_aligned_path = dataset_name + '_word_aligned_highlevel'
-    if os.path.isdir(word_aligned_path) and os.listdir(word_aligned_path) != []:
-        dataset["highlevel"] = mmdatasdk.mmdataset(word_aligned_path)
-    else:
-        dataset["highlevel"].align("glove_vectors")
-        dataset["highlevel"].impute('glove_vectors')
-        deploy(dataset["highlevel"], word_aligned_path)
-
-    final_aligned_path = dataset_name + '_final_aligned_highlevel'
+    final_aligned_path = folder_path + '_final_aligned_highlevel'
     if os.path.isdir(final_aligned_path) and os.listdir(final_aligned_path) != []:
-        dataset["highlevel"] = mmdatasdk.mmdataset("final_aligned")
+        dataset["highlevel"] = mmdatasdk.mmdataset(final_aligned_path)
+
     else:
+        word_aligned_path = folder_path + '_word_aligned_highlevel'
+        if os.path.isdir(word_aligned_path) and os.listdir(word_aligned_path) != []:
+            dataset["highlevel"] = mmdatasdk.mmdataset(word_aligned_path)
+        else:
+            dataset["highlevel"].align("glove_vectors")
+            dataset["highlevel"].impute('glove_vectors')
+            deploy(dataset["highlevel"], word_aligned_path)
+
         dataset["highlevel"].computational_sequences["All Labels"] = dataset["labels"]["All Labels"]
 
         dataset["highlevel"].align("All Labels")
         dataset["highlevel"].hard_unify()
-
         deploy(dataset["highlevel"], final_aligned_path)
 
     tensors = dataset["highlevel"].get_tensors(
@@ -117,7 +123,16 @@ def process_data(dataset_name, seq_len=50):
 if __name__=="__main__":
 
     print("You only need to download the data once!")
-    dataset_name = "cmumosei"
+    supported_datasets = ["cmumosei", "cmumosi", "pom"]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--dataset_name', required=True,
+                        type=str, nargs='+', help='Dataset name from possible (cmumosei, cmumosi and pom)')
+    args = parser.parse_args()
+
+    dataset_name = args.dataset_name[0]
+    if dataset_name not in supported_datasets:
+        raise ValueError("Unsupported dataset. Only supported datasets are cmumosei, cmumosi and pom")
+
     dataset = download_data(mmdatasdk.cmu_mosei, dataset_name)
     process_data(dataset_name)
     log.success("Dataset processed")
