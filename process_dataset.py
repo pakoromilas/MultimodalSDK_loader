7047da53c29ac845a9f418edfe0b175ab4b23ddc
lobracost <pa.koromilas@gmail.com>
2021-09-15 19:17:30 +0300

    minor

--- process_dataset.py
+++ process_dataset.py
@@ -77,7 +77,7 @@ def process_data(dataset_name, seq_len=50, non_sequences=["All Labels"]):
     dataset={}
     for folder in folders:
         dataset[folder.split("_")[1]] = mmdatasdk.mmdataset(folder)
-    
+
     word_aligned_path = dataset_name + '_word_aligned_highlevel'
     if os.path.isdir(word_aligned_path) and os.listdir(word_aligned_path) != []:
         dataset["highlevel"] = mmdatasdk.mmdataset(word_aligned_path)
@@ -87,7 +87,7 @@ def process_data(dataset_name, seq_len=50, non_sequences=["All Labels"]):
         deploy(dataset["highlevel"], word_aligned_path)
 
     final_aligned_path = dataset_name + '_final_aligned_highlevel'
-    if os.path.isdir(word_aligned_path) and os.listdir(word_aligned_path) != []:
+    if os.path.isdir(final_aligned_path) and os.listdir(final_aligned_path) != []:
         dataset["highlevel"] = mmdatasdk.mmdataset("final_aligned")
     else:
         dataset["highlevel"].computational_sequences["All Labels"] = dataset["labels"]["All Labels"]
