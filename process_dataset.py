9a24bb1a18e11726858b61578d5cd8cd43be2a9f
lobracost <pa.koromilas@gmail.com>
2021-09-16 00:37:17 +0300

    working version for cmumosei + cmumosi

--- process_dataset.py
+++ process_dataset.py
@@ -81,40 +81,44 @@ def download_data(dataset_config, dataset_name):
 
 def process_data(dataset_name, seq_len=50):
 
-    non_sequences = [feature_selection[dataset_name]["labels"]]
-
     dataset_path = os.path.join("data", dataset_name)
     folder_path = os.path.join(dataset_path, dataset_name)
-    folders = [folder_path + "_highlevel", folder_path + "_labels"]
 
-    dataset={}
-    for folder in folders:
-        dataset[folder.split("_")[1]] = mmdatasdk.mmdataset(folder)
+    dataset = {}
 
+    dataset["labels"] = mmdatasdk.mmdataset(folder_path + "_labels")
+
+    word_aligned_path = folder_path + '_word_aligned_highlevel'
     final_aligned_path = folder_path + '_final_aligned_highlevel'
+
     if os.path.isdir(final_aligned_path) and os.listdir(final_aligned_path) != []:
         dataset["highlevel"] = mmdatasdk.mmdataset(final_aligned_path)
 
     else:
-        word_aligned_path = folder_path + '_word_aligned_highlevel'
         if os.path.isdir(word_aligned_path) and os.listdir(word_aligned_path) != []:
             dataset["highlevel"] = mmdatasdk.mmdataset(word_aligned_path)
+
         else:
+            dataset["highlevel"] = mmdatasdk.mmdataset(folder_path + "_highlevel")
+
             dataset["highlevel"].align("glove_vectors")
             dataset["highlevel"].impute('glove_vectors')
             deploy(dataset["highlevel"], word_aligned_path)
 
-        dataset["highlevel"].computational_sequences["All Labels"] = dataset["labels"]["All Labels"]
+        dataset["highlevel"].computational_sequences[feature_selection[dataset_name]["labels"]] = \
+            dataset["labels"][feature_selection[dataset_name]["labels"]]
 
-        dataset["highlevel"].align("All Labels")
+        dataset["highlevel"].align(feature_selection[dataset_name]["labels"])
         dataset["highlevel"].hard_unify()
         deploy(dataset["highlevel"], final_aligned_path)
 
+    non_sequences = [feature_selection[dataset_name]["labels"]]
+
     tensors = dataset["highlevel"].get_tensors(
         seq_len=seq_len, non_sequences=non_sequences, direction=False,
-        folds=[mmdatasdk.cmu_mosei.standard_folds.standard_train_fold,
-               mmdatasdk.cmu_mosei.standard_folds.standard_valid_fold,
-               mmdatasdk.cmu_mosei.standard_folds.standard_test_fold])
+        folds=[dataset_configs[dataset_name].standard_folds.standard_train_fold,
+               dataset_configs[dataset_name].standard_folds.standard_valid_fold,
+               dataset_configs[dataset_name].standard_folds.standard_test_fold])
 
     fold_names = ["train", "valid", "test"]
 
